45e2ab30b1abf9d17ad2fb59c48e758d1d02b3f9
Lyy <liyunyao1010@163.com>
2018-05-27 20:55:17 +0800

    first

---
+++ system_object.py
@@ -0,0 +1,23 @@
+#
+
+#related object
+# May,26,2018 by Lyy
+# Movies Object
+
+class Movies(object):
+    def __init__(object, Mid=-1, user_Socre=-1):
+        self.Mid = Mid
+        self.user_Socre = Socre
+        self.averay_socre
+        self.Name = ''
+
+class User(object):
+    def __init__(object, ID, movies):
+        self.ID = ID
+        self.movies = movies
+
+class MoviesDetail(object):
+    def __init__(object):
+        self.movies = Movies()
+        self.story = ''
+        self.kind = ''
