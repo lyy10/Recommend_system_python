bfeebcdb18378a53d7f572f29e9818cd5b1816b4
chenghui <deadline20161225@163.com>
2018-06-20 13:39:55 +0800

    update routes.py

--- web_recommend/flask-gentelella/source/home/routes.py
+++ web_recommend/flask-gentelella/source/home/routes.py
@@ -42,20 +42,38 @@ def recommend():
     user = interface.get_recommend_movie(int(current_user.ID))
     movie_id_list = []
     movie_name_list = []
-    movie_averay_socre_list = []
+    movie_average_score_list = []
+    movie_post_list = []
     for each in user.movies:
         movie_id_list.append(each.Mid)
         movie_name_list.append(each.Name)
-        movie_averay_socre_list.append(each.averay_socre)
+        movie_average_score_list.append(round(each.average_score, 2))
+        movie_post_list.append(each.post)
     count = len(movie_id_list)
     count = count//5
-    return render_template('recommend.html', movie_id_list=movie_id_list, movie_name_list=movie_name_list, movie_averay_socre_list=movie_averay_socre_list, count=count)
+    return render_template('recommend.html', movie_id_list=movie_id_list, movie_name_list=movie_name_list, movie_average_score_list=movie_average_score_list, movie_post_list=movie_post_list, count=100)
 
 
-@blueprint.route('/movie_details')
+@blueprint.route('/movie_details/<movie_id>')
 @login_required
-def movie_details():
-    return render_template('movie_details.html')
+def movie_details(movie_id):
+    movie = interface.getMovieDetail(movie_id)
+    movie_id = movie_id
+    movie_name = movie.base.Name
+    movie_post = movie.base.post
+    movie_director = movie.director
+    movie_creator = movie.creator
+    movie_stars = movie.stars
+    movie_kind = movie.kind
+    movie_country = movie.country
+    movie_language = movie.language
+    movie_runtime = movie.runtime
+    movie_score = movie.base.average_score
+    movie_story = movie.story
+    movie_url = movie.url
+    return render_template('movie_details.html', movie_id=movie_id, movie_name=movie_name,movie_director=movie_director,movie_creator=movie_creator,
+                           movie_stars=movie_stars,movie_kind=movie_kind,movie_country=movie_country,movie_language=movie_language,
+                           movie_runtime=movie_runtime,movie_score=movie_score,movie_story=movie_story,movie_url=movie_url,movie_post=movie_post)
 
 
 @blueprint.route('/score', methods=['POST'])
@@ -69,4 +87,17 @@ def score():
 @blueprint.route('/watched_movies')
 @login_required
 def watched_movies():
-    return render_template('watched_movies.html')
+    user = interface.getUserHaveWatch(int(current_user.ID))
+    movie_id_list = []
+    movie_name_list = []
+    movie_average_score_list = []
+    movie_post_list = []
+    for each in user.movies:
+        movie_id_list.append(each.Mid)
+        movie_name_list.append(each.Name)
+        movie_average_score_list.append(round(each.average_score, 2))
+        movie_post_list.append(each.post)
+    count = len(movie_id_list)
+    count = count // 5
+    return render_template('watched_movies.html', movie_id_list=movie_id_list,movie_post_list=movie_post_list,movie_name_list=movie_name_list,
+                           movie_average_score_list=movie_average_score_list,count=count)
