b068f6562fc259ab32df81c10c8f7cbc220e1c9b
chenghui <deadline20161225@163.com>
2018-06-26 20:09:33 +0800

    delete

--- web_recommend/test.py
+++ web_recommend/test.py
@@ -6,9 +6,14 @@ import interface
 print(interface.accessCheck('1', '1'))
 print(type(interface.accessCheck('1', '1')))
 
-user = interface.getUserHaveWatch(21)
+user = interface.get_recommend_movie(21)
 movie_id_list = []
 for each in user.movies:
     movie_id_list.append(each.Mid)
 
+watched_movie = interface.getUserHaveWatch(2)
+watched_movie_id_list = []
+for each in user.movies:
+    watched_movie_id_list.append(each.Mid)
+print(len(watched_movie_id_list))
 print(len(movie_id_list))
